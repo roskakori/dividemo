021827dd386f2302181312ebeea2a90dc62a11e1
Thomas Aglassinger <roskakori@users.sourceforge.net>
2015-04-25 12:39:02 +0200

    Added initial version.

---
+++ dividemo/dividemo.py
@@ -0,0 +1,23 @@
+"""
+dividemo - tool to divide two integer numbers.
+"""
+import argparse
+import math
+import sys
+
+def divided(dividend, divisor):
+    return dividend // divisor  # //=integer division
+
+def main(arguments=None):
+    # Parse command line arguments.
+    parser = argparse.ArgumentParser(description='divide two integer numbers.')
+    parser.add_argument('dividend', metavar='DIVIDEND', type=int, help='number to divide')
+    parser.add_argument('divisor',  metavar='DIVISOR', type=int,help='number to divide by')
+    args = parser.parse_args(arguments)
+
+    # Process arguments and print result.
+    result = divided(args.dividend, args.divisor)
+    print(result)
+
+if __name__ == '__main__':
+    main()
